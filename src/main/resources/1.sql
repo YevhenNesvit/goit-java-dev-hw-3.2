5744de7b3aab0628696cf8fd9b7c66d49b9b82f2
Yevhen <yevhen.nesvit@gmail.com>
2022-08-26 11:58:15 +0300

    Initial commit

---
+++ src/main/resources/1.sql
@@ -0,0 +1,10 @@
+ALTER TABLE developers ADD COLUMN salary int;
+
+UPDATE developers SET salary = 3000 WHERE last_name = 'Chung';
+UPDATE developers SET salary = 7000 WHERE last_name = 'de Haan';
+UPDATE developers SET salary = 1000 WHERE last_name = 'Lee';
+UPDATE developers SET salary = 5000 WHERE last_name = 'Nayer';
+UPDATE developers SET salary = 2000 WHERE last_name = 'Nesvit';
+UPDATE developers SET salary = 3000 WHERE last_name = 'Onyschenko';
+UPDATE developers SET salary = 2000 WHERE last_name = 'Seo';
+UPDATE developers SET salary = 4000 WHERE last_name = 'Sydorchuk';
